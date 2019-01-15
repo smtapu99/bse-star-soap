4321a9b8a23e0c07c765d4358c4578c2de8c37a3
smtapu99 <shahadatmahmud99@gmail.com>
2019-01-14 17:24:23 -0800

    routes method to post

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Login
-  get 'get_password', to: 'get_password#get_password_response', as: :get_password_response
+  post 'get_password', to: 'get_password#get_password_response', as: :get_password_response
 end
