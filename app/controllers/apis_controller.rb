ed87f0692590605537b34ffc9142b81767146136
smtapu99 <shahadatmahmud99@gmail.com>
2019-01-16 22:59:18 -0800

    transaction

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -1,5 +1,6 @@
 class ApisController < ApplicationController
 
+  include TransactionHelper
   ################ SOAP FUNCTIONS to get/post data - called by MAIN FUNCTIONS
 
   ## fire SOAP query to get password for Order API endpoint
@@ -32,19 +33,19 @@ class ApisController < ApplicationController
 
     # Respond converted json response
     if result_code.to_i == 100
-      result_message
-      # render status: 200, json: {
-      #     status: "success",
-      #     response_code: result_code,
-      #     auth_token: result_message  # if success, result_message will be "new password"
-      # }.to_json
+      # result_message
+      render status: 200, json: {
+          status: "success",
+          response_code: result_code,
+          auth_token: result_message  # if success, result_message will be "new password"
+      }.to_json
     else
-      raise result_message
-      # render status: 200, json: {
-      #     status: "fail",
-      #     code: result_code,
-      #     message: result_message # if failed, result_message will be alert message
-      # }.to_json
+      # raise result_message
+      render status: 200, json: {
+          status: "fail",
+          code: result_code,
+          message: result_message # if failed, result_message will be alert message
+      }.to_json
     end
   end
 
@@ -55,34 +56,62 @@ class ApisController < ApplicationController
     # create a client for the service
     client = get_soap_client(soap_header)
 
-    soap_actions = client.operations
-    write_log('soap_actions', soap_actions.inspect)
     data = {
-        "UserId" => params[:user_id],
-        "Password" => params[:password],
-        "PassKey" => params[:pass_key]
+      "TransCode" => params[:trans_code].try(:upcase), #NEW/MOD/CXL, Order : New/Modification/Cancellation
+      "TransNo" => generate_trans_no(''), #trans_no is a unique number sent with every transaction creation request sent to BSE
+      "OrderId" => params[:order_id], #BSE unique order number, for new order this field will be blank and in case of  modification and  cancellation the order  number has to be given
+      "UserID" => params[:user_id],
+      "MemberId" => params[:member_id],#Member code as given by BSE
+      "ClientCode" => params[:client_code],
+      "SchemeCd" => SecureRandom.hex,#BSE scheme code
+      "BuySell" => params[:buy_sell].try(:upcase), #P/R Type of transaction i.e.Purchase or Redemption
+      "BuySellType" => params[:buy_sell_type].try(:upcase), #Buy/Sell type i.e. fresh or additional. FRESH/ADDITIONAL
+      "DPTxn" => params[:dptxn].try(:upcase), #C/N/P, CDSL/NSDL/PHYSICAL
+      "OrderVal" => params[:all_redeem].try(:downcase) == 'y' ? '': params[:amount], # Purchase/Redemption amount(redemption amount only incase of physical redemption)
+      "Qty" => params[:all_redeem].try(:downcase) == 'y' ? '': params[:amount], # Redemption quantity
+      "AllRedeem" => params[:all_redeem].try(:upcase), # All units flag, If this Flag is "Y" then units and amount column should be blank
+      "FolioNo" => '',#Incase demat transaction this field will be blank and mandatory in case of physical redemption and purchase+additional
+      "Remarks" => '',
+      "KYCStatus" => params[:kyc_status].try(:upcase), # Y/N, KYC status of client
+      "RefNo" =>'', #Internal reference number
+      "SubBrCode" => params[:sub_br_code],# Sub Broker code
+      "EUIN" => params[:euin], # EUIN number
+      "EUINVal" => params[:euin_val],
+      "MinRedeem" => params[:min_redeem].try(:upcase), #Y/N,  Minimum redemption flag
+      "DPC" => params[:dpc].try(:upcase), #Y/N, DPC flag for purchase transactions
+      "IPAdd" =>'',
+      "Password" => params[:password],
+      "PassKey" => params[:pass_key],
+      "Parma1" => params[:param_1],
+      "Parma2" => params[:param_2],
+      "Parma3" => params[:param_3]
     }
 
     response = call_soap_client(client, 'order_entry_param', data)
     hash = response.hash
     result_string = hash['envelope']['body']['order_entry_param_response']['order_entry_param_result']
 
-    result_code = result_string.split('|').first
-    result_message = result_string.split('|').last
+    result = result_string.split('|')
 
     # Respond converted json response
-    if result_code.to_i == 100
+    if result.last.to_i == 0
       render status: 200, json: {
           status: "success",
-          response_code: result_code,
-          auth_token: result_message  # if success, result_message will be "new password"
+          transaction_code: result[0],
+          unique_reference_number: result[1],
+          order_number: result[2],
+          user_id: result[3],
+          member_id: result[4],
+          client_code: result[5],
+          bse_remarks: result[6],
+          success_flag: result.last
       }.to_json
     else
-      raise result_message
+      # raise result_message
       render status: 200, json: {
           status: "fail",
-          code: result_code,
-          message: result_message # if failed, result_message will be alert message
+          success_flag: result.last,
+          bse_remarks: result[6] # if failed, result_message will be alert message
       }.to_json
     end
   end
--- app/helpers/transaction_helper.rb
+++ app/helpers/transaction_helper.rb
@@ -1,4 +1,7 @@
 module TransactionHelper
 
-
+  #Unique Ref no-  numeric, length must be short than 20, YYYYMMDD<usercode>000001
+  def generate_trans_no(trans_type, user_id = 1, last_ref_no = 1)
+    trans_no_header = "#{Time.now.strftime('%Y%m%d')}#{user_id.to_s}#{(last_ref_no.to_i + 1).to_s}"
+  end
 end
