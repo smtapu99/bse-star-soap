26cb4d960d43a34b30e739a9b5b8ba4b92b4a2ed
smtapu99 <shahadatmahmud99@gmail.com>
2019-02-11 18:03:16 -0800

    downgrade to ruby 2.2.2

--- app/helpers/transaction_helper.rb
+++ app/helpers/transaction_helper.rb
@@ -2,7 +2,7 @@ module TransactionHelper
 
   #Unique Ref no-  numeric, length must be short than 20, YYYYMMDD<usercode>000001
   def generate_trans_no(trans_type, user_id = 1, last_ref_no = 1)
-    seed = SecureRandom.rand(10000) % 100
+    seed = Random.rand(10000) % 100
     "#{Time.now.strftime('%Y%m%d')}#{user_id.to_s}#{(last_ref_no.to_i + 1).to_s}#{seed}"
   end
 end
